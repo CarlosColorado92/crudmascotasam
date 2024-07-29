3ee0e2e5abcd2cdc7627503f96a16d0d4b798d42
Carlos Colorado <carlosteei1992@gmail.com>
2024-07-29 16:19:38 -0500

    Barra de navegacion y enrutamiento

--- app.js
+++ app.js
@@ -9,16 +9,10 @@ app.set('views', __dirname + '/views');
 /* middleware para contenido estatico */ 
 app.use(express.static(__dirname + '/public'));
 
-/* enrutamiento */
-app.get('/', (req, res) => {
-//   res.send('!!! Hola Mundo !!!')
-    res.render('index', {titulo: "Bienvenido a Node.js con Express y con EJS"});
-})
+/* enrutamiento del proyecto  */
+app.use('/', require('./router/rutasweb'));
+app.use('/mascotas', require('./router/mascotas'));
 
-app.get("/servicios", (req, res)=>{
-    // res.send("Bienvenido a la página de servicios");
-    res.render('servicios', {titulo: "Nuestros Servicios Dinámicos"});
-  });
 
 /* direccionar a vista 404 cuando se presente el error 404 */
 app.use((req, res, next)=>{
