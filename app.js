e6395db2181ec9eeee7286640ea6036f65fb0606
Carlos Colorado <carlosteei1992@gmail.com>
2024-07-29 16:30:18 -0500

    Inicio modulo mascotas

--- app.js
+++ app.js
@@ -25,3 +25,7 @@ app.use((req, res, next)=>{
 app.listen(port, () => {
   console.log(`Ejemplo aplicación Node con Express escuchando en puerto ${port}`);
 })
+
+app.listen(port, () => {
+  console.log(`Ejemplo aplicación Node con Express escuchando en puerto ${port}`);
+})
